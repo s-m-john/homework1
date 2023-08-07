10e54d58e104a508a8d4a1b57e76246362ed2132
Sophia John <smc_john@hotmail.com>
2023-08-06 20:11:12 -0400

    styling

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function calculateSum() {
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
-  
+    
     if (isNaN(num1) || isNaN(num2)) {
-      document.getElementById("result").innerText = "Please enter valid numeric values.";
+      document.getElementById("result").textContent = "Please enter valid numbers";
     } else {
       const sum = num1 + num2;
-      document.getElementById("result").innerText = "Sum: " + sum;
+      document.getElementById("result").textContent = `Sum: ${sum}`;
     }
   }
   
